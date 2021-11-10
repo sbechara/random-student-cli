fcf7775c956d186e9185403cd3bcff9e7582af63
Sam Bechara <sbechara@bechara-macbook.local>
2021-11-09 22:40:23 -0700

    Fixing the excel file and updating initialize

--- initializeRandomStudent.m
+++ initializeRandomStudent.m
@@ -1,49 +1,26 @@
-% Copyright (C) 2018 Samuel Bechara
-% 
-% This program is free software; you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 3 of the License, or
-% (at your option) any later version.
-% 
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
+function [courseIdent] = initializeRandomStudent (xlsFileName)
+% initializeRandomStudent() is meant to be run once at the beginning of each
+% semester to generate the vectors and .mat files necessary for the
+% randomStudent functions to work
+%
+% PLEASE SEE DOCS FOR ADDITIONAL INFORMATION
+%
+% Inputs:
+%   xlsFileName - a string that points to an xls file with the names of
+%   all the students in the section. Note: the name of the xlsFileName
+%   will become the name of the course.
+% Created: 2018-01-05
+% Last Edited: 9-Nov-2021,2018-01-16
 
-% Author: Samuel Bechara <sbechara@sbechara-ThinkPad-T440s>
-% Created: 2018-01-04
+courseIdent = readtable(xlsFileName);
+courseIdent.NumCalls = zeros(length(courseIdent.Names),1);
+courseIdent.NumCorrect = courseIdent.NumCalls;
+courseIdent.NumWrong = courseIdent.NumCalls;
+courseIdent.Properties.RowNames = courseIdent.Names;
 
-function initializeRandomStudent (xlsFileName, courseIdent)
-  % initializeRandomStudent() is meant to be run once at the beginning of each
-  % semester to generate the vectors and .mat files necessary for the
-  % randomStudent functions to work
-  %
-  % PLEASE SEE DOCS FOR ADDITIONAL INFORMATION
-  %
-  % Inputs:
-  %   csvFileName - a string that points to semicolon delimited csv file
-  %   courseIdent - user chooses a name to identify course by. Will be .mat file used in all future calls to functions.
-  %                 i.e.) mech105
-  % initialize blacklist to 0 since no one drops before classes start
-  % Author: Samuel Bechara <sbechara@sbechara-ThinkPad-T440s>
-  % Created: 2018-01-05
-  % Last Edited: 2018-01-16
-  
-  blacklist = [0]; 
-  
-  % Need to fix doesn't work with MATLAB for some reason
-  %names = textread(csvFileName,'%s','delimiter',';');
-  data = readtable(xlsFileName);
-  names = data.Names;
-  calls = zeros(length(names),1);
-  numCorrect = calls;
-  
-  currentDirectory = pwd;
-  saveFilePath = strcat(currentDirectory,"/",courseIdent,".mat");
-  
-  % only save the necessary workspace variables
-  save(saveFilePath,'blacklist','names','calls','numCorrect','saveFilePath');
-  
-  fprintf("\nNote the save path is %s\n",saveFilePath);
+% only save the necessary workspace variables
+save(xlsFileName,'-mat');
+
+fprintf("\n -------\n Success \n -------\n");
 
 end
