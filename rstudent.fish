ed3bd2402add66bb07861d29d7e683ca7917197e
Samuel Bechara <sbechara@proton.me>
2022-04-18 15:11:02 -0600

    Edited fish function

--- rstudent.fish
+++ rstudent.fish
@@ -1,4 +1,3 @@
-## Ok now what?
 # This is a fish function to automatically change directory and launch the rstudent cli
 function rstudent
     cd /Users/sbechara/Nextcloud/CSU/Software/random-student-cli/
