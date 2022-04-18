84b9ea973b533b7b7591eb4d5a5f6008ccc34bd5
Samuel Bechara <sbechara@proton.me>
2022-04-18 15:03:26 -0600

    Edited README

--- rstudent.fish
+++ rstudent.fish
@@ -1,6 +1,6 @@
 ## Ok now what?
 # This is a fish function to automatically change directory and launch the rstudent cli
 function rstudent
-              cd /Users/sbechara/Nextcloud/CSU/Software/random-student-cli/
-              /Applications/MATLAB_R2022a.app/bin/matlab -nodesktop
+    cd /Users/sbechara/Nextcloud/CSU/Software/random-student-cli/
+    /Applications/MATLAB_R2022a.app/bin/matlab -nodesktop
 end
